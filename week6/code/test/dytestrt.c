3fd504d90b171347d3a9b04fe41db7351d569b6a
lks <1046675257@qq.com>
2019-10-23 02:14:38 -0700

    a

---
+++ week6/code/test/dytestrt.c
@@ -0,0 +1,52 @@
+#include"dylib.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<dlfcn.h>
+int main()
+{
+  void *hd;
+  void (*f1)(),(*f2)();
+  int (*f3)(),(*f4)();
+  char *error;
+  int a[5]={12,0,-154,7668,334};
+  hd=dlopen("../dynamiclib/libdylib.so",RTLD_LAZY);
+  if(!hd){
+  fprintf(stderr,"%s\n", dlerror());
+  exit(1);
+}
+f1=dlsym(hd,"init");
+  if((error=dlerror())!=NULL)
+{
+  fprintf(stderr,"%s\n",dlerror());
+  exit(1);
+}
+  f2=dlsym(hd,"show");
+  
+  if((error=dlerror())!=NULL)
+{
+  fprintf(stderr,"%s\n",dlerror());
+  exit(1);
+}
+  f3=dlsym(hd,"max");
+   if((error=dlerror())!=NULL)
+{
+  fprintf(stderr,"%s\n",dlerror());
+  exit(1);
+}
+  f4=dlsym(hd,"sum");
+   if((error=dlerror())!=NULL)
+{
+  fprintf(stderr,"%s\n",dlerror());
+  exit(1);
+}
+  f1(a,5);
+  f2(a,5);
+  printf("   max = %4d\n", f3(a,5));
+  printf("   sum = %4d\n", f4(a,5));
+  if(dlclose(hd)<0){
+    fprintf(stderr,"%s\n",dlerror());
+  exit(1);
+}
+  return 0;
+  
+}
